7fb334228bd5484f6c3da9b1c0cb5801706ada40
kidiga <juniorprogrammer09@gmail.com>
2024-12-02 13:11:23 +0300

    initial security configuration bean

---
+++ booker/src/main/java/io/github/junrdev/booker/sec/SecurityConfig.java
@@ -0,0 +1,11 @@
+package io.github.junrdev.booker.sec;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    //provide filter chain bean
+
+
+}
