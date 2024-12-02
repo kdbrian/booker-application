d71d8ce7a4da7d6d81c6f15408a688e2b2041dd9
kidiga <juniorprogrammer09@gmail.com>
2024-12-02 13:28:53 +0300

    a core impl of core functionalities

---
+++ booker/src/main/java/io/github/junrdev/booker/controller/rest/CoreController.java
@@ -0,0 +1,21 @@
+package io.github.junrdev.booker.controller.rest;
+
+import io.github.junrdev.booker.domain.enumarations.PAYMENT_STATUS;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/core")
+public class CoreController {
+
+    @GetMapping("/payment-statuses")
+    public ResponseEntity<PAYMENT_STATUS[]> getPaymentStatusValues(){
+        var statuses = PAYMENT_STATUS.values();//.map(Enum::name);
+        return ResponseEntity.ok(statuses);
+    }
+}
