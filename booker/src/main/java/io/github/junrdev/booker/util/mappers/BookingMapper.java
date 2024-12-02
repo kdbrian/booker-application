4c0b568e75e4bd6ae1b9595ae42d4fc3ec544f15
kidiga <juniorprogrammer09@gmail.com>
2024-12-02 04:27:07 +0300

    update booking mapper

--- booker/src/main/java/io/github/junrdev/booker/util/mappers/BookingMapper.java
+++ booker/src/main/java/io/github/junrdev/booker/util/mappers/BookingMapper.java
@@ -1,9 +1,13 @@
 package io.github.junrdev.booker.util.mappers;
 
 import io.github.junrdev.booker.domain.dto.BookingDto;
+import io.github.junrdev.booker.domain.enumarations.BOOKING_STATUS;
 import io.github.junrdev.booker.domain.enumarations.PAYMENT_STATUS;
 import io.github.junrdev.booker.domain.model.Booking;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class BookingMapper extends EntityToDtoMapper<Booking, BookingDto> {
@@ -11,10 +15,10 @@ public class BookingMapper extends EntityToDtoMapper<Booking, BookingDto> {
     @Override
     public Booking fromDto(BookingDto bookingDto) {
         return Booking.builder()
-                .userID(bookingDto.getUserId())
-                .createdAt(bookingDto.getCreatedAt())
-                .paymentStatus(bookingDto.getPaymentStatus())
-                .bookingStatus(bookingDto.getBookingStatus())
+                .userID(bookingDto.getUserId()!=null?bookingDto.getUserId(): UUID.randomUUID().toString())
+                .createdAt(bookingDto.getCreatedAt()!=null?bookingDto.getCreatedAt(): LocalDateTime.now().toString())
+                .paymentStatus(bookingDto.getPaymentStatus()!=null?bookingDto.getPaymentStatus():PAYMENT_STATUS.PENDING)
+                .bookingStatus(bookingDto.getBookingStatus()!=null?bookingDto.getBookingStatus(): BOOKING_STATUS.PENDING)
                 .build();
     }
 
