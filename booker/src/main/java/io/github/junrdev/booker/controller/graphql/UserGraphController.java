9a336812181b4ba0602fc3462d47e62d3da2a653
kidiga <juniorprogrammer09@gmail.com>
2024-12-04 21:10:12 +0300

    update schema controller for users

--- booker/src/main/java/io/github/junrdev/booker/controller/graphql/UserGraphController.java
+++ booker/src/main/java/io/github/junrdev/booker/controller/graphql/UserGraphController.java
@@ -1,31 +1,50 @@
 package io.github.junrdev.booker.controller.graphql;
 
 
+import io.github.junrdev.booker.domain.dto.CompanyDTO;
 import io.github.junrdev.booker.domain.dto.UserDto;
 import io.github.junrdev.booker.domain.model.AppUser;
 import io.github.junrdev.booker.domain.model.Booking;
 import io.github.junrdev.booker.domain.service.UserService;
+import io.github.junrdev.booker.util.error.AppError;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserGraphController {
 
     private final UserService userService;
+    private final Validator validator;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserGraphController.class);
 
     @Autowired
-    public UserGraphController(UserService userService) {
+    public UserGraphController(UserService userService, Validator validator) {
         this.userService = userService;
+        this.validator = validator;
     }
 
     @MutationMapping
     public AppUser addUser(@Valid @Argument UserDto dto) {
+
+        Set<ConstraintViolation<UserDto>> violations = validator.validate(dto);
+        if (!violations.isEmpty()) {
+            String validatorError = violations.stream().map(v -> "\n" + v.getMessage()).collect(Collectors.joining());
+            throw new AppError("Failed to validate request : Error \n" + validatorError, HttpStatus.BAD_REQUEST);
+        }
         return userService.addUser(dto);
     }
 
@@ -51,6 +70,12 @@ public class UserGraphController {
     public String deleteUsers() {
         var record = userService.deleteUsers();
         return String.format("Deleted %d records.", record);
+    }
+
+
+    @MutationMapping
+    public Boolean deactivateUser(@Argument String userId) {
+        return userService.deactivateUser(userId) != null;
     }
 
     @QueryMapping
