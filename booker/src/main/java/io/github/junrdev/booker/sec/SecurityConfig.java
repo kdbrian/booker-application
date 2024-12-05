9fe3358d9bdf54ff4c345b13f267509a97797eb4
kidiga <juniorprogrammer09@gmail.com>
2024-12-05 14:49:07 +0300

    add the user details service bean with an in memory configured user  and authorities admin

--- booker/src/main/java/io/github/junrdev/booker/sec/SecurityConfig.java
+++ booker/src/main/java/io/github/junrdev/booker/sec/SecurityConfig.java
@@ -2,9 +2,14 @@ package io.github.junrdev.booker.sec;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +22,17 @@ public class SecurityConfig {
 //        return new BCryptPasswordEncoder();
 //    }
 
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("admin")
+                .password("2022")
+                .authorities("read")
+                .build();
+
+        userDetailsManager. createUser(userDetails);
+        return userDetailsManager;
+    }
 
     @GetMapping("/hello")
     private String hello(){
