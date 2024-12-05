905d2f1ed3f380b060d088d4f07fbae9e7042aa1
kidiga <juniorprogrammer09@gmail.com>
2024-12-05 03:35:04 +0300

    all changes

--- booker/src/main/java/io/github/junrdev/booker/sec/SecurityConfig.java
+++ booker/src/main/java/io/github/junrdev/booker/sec/SecurityConfig.java
@@ -1,4 +1,27 @@
 package io.github.junrdev.booker.sec;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecurityConfig {
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+
+
+    @GetMapping("/hello")
+    private String hello(){
+        return "Hello!";
+    }
+
+
 }
