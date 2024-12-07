95d21a6df98ef73929c69ec3d6bf9b4e615b184c
kidiga <juniorprogrammer09@gmail.com>
2024-12-07 19:19:43 +0300

    disable csrf allowing post methods

--- booker/src/main/java/io/github/junrdev/booker/config/SecurityConfig.java
+++ booker/src/main/java/io/github/junrdev/booker/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,8 +28,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/auth/users/**", "/api/**").permitAll()
+                        req.requestMatchers(
+                                        "/auth/users/register",
+                                        "/auth/users/login",
+                                        "/auth/users/**",
+                                        "/api/**"
+                                ).permitAll()
                                 .anyRequest().authenticated()
                 )
                 //setting the auth provider(bean in app config)
