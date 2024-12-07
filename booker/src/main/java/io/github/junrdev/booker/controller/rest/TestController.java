37f2114078d3b7cd34156babea19a4f78bb2c040
kidiga <juniorprogrammer09@gmail.com>
2024-12-07 14:33:31 +0300

    update test controller

--- booker/src/main/java/io/github/junrdev/booker/controller/rest/TestController.java
+++ booker/src/main/java/io/github/junrdev/booker/controller/rest/TestController.java
@@ -1,13 +1,21 @@
 package io.github.junrdev.booker.controller.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/tests")
 public class TestController {
 
-    @GetMapping("/api/test")
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/no-auth")
     public String amATeaPot() {
+        log.info("called");
         return "Hi, I am a tea pot.";
     }
 }
