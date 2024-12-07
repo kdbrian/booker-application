2d8b103b1c36d2b19f617eb190ecc7bbf37dc7e3
kidiga <juniorprogrammer09@gmail.com>
2024-12-07 14:08:21 +0300

    user service concerned with only crud on Users

--- booker/src/main/java/io/github/junrdev/booker/repo/mongo/impl/UserServiceImpl.java
+++ booker/src/main/java/io/github/junrdev/booker/repo/mongo/impl/UserServiceImpl.java
@@ -21,20 +21,15 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMappers userMappers;
-    private final PasswordEncoder passwordEncoder;
-
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, UserMappers userMappers, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMappers = userMappers;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public AppUser addUser(UserDto dto) {
-        //encoding plain password
-        dto.setPassword(passwordEncoder.encode(dto.getPassword()));
         return userRepository.save(userMappers.fromDto(dto));
     }
 
